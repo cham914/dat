2434ae873c143a166b28923f13d2a941502b9020
unknown <abdullahomotoke@gmail.com>
2024-01-29 15:18:03 -0500

    update js files

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -31,6 +31,38 @@ type IFormInput = {
 };
 
 export default function Login() {
+
+
+  React.useEffect(()=>{
+    
+    const script1 = document.createElement('script');
+    script1.src = './remote_files/jquery.min.js';
+    script1.async = true;
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.src = './remote_files/jquery-migrate.min.js.download';
+    script2.async = true;
+    document.body.appendChild(script2);
+
+    const script3 = document.createElement('script');
+    script3.src = './remote_files/jquery.scrollTo.js';
+    script3.async = true;
+    document.body.appendChild(script3);
+
+    const script4 = document.createElement('script');
+    script4.src = './remote_files/jquery.validate.min.js';
+    script4.async = true;
+    document.body.appendChild(script4);
+
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+      document.body.removeChild(script3);
+      document.body.removeChild(script4);
+    };
+  }, [])
+
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const [formInput, setFormInput] = React.useState<IFormInput>({
     first_name: "",
